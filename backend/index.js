d292a541ece876a15408d7932963585f9ea0f78f
saurabh-develop <saurabhkhare893@gmail.com>
2026-03-24 12:43:31 +0530

    Mongodb and index.js setup for backend

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import router from "./routes/routes.js";
+import connectDB from "./db/db.js";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8001;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/v1", router);
+
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server is listening to the port: ${PORT}`);
+});
